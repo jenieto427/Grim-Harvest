12d0fdec3e63d5bc9a19f397c99d46a7fedb6357
michaelp15 <123104637+michaelp15@users.noreply.github.com>
2024-04-04 19:56:24 -0600

    Minigame Progress

--- Assets/Scripts/UI Scripts/Minigame Scripts/Minigame Manager.cs
+++ Assets/Scripts/UI Scripts/Minigame Scripts/Minigame Manager.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MinigameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MinigameManager Instance { get; private set; }
+
+    public GameObject minigameUI; // Assign this in the Inspector
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartMinigame()
     {
-        
+        minigameUI.SetActive(true); // Show the minigame UI
+        // Any other initialization for the minigame can be done here
+    }
+
+    public void EndMinigame()
+    {
+        minigameUI.SetActive(false); // Hide the minigame UI
+        // Clean up the minigame, save progress, etc.
     }
 }
