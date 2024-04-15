de25df1316fc725f3665bb0b87740a123e9e2472
michaelp15 <123104637+michaelp15@users.noreply.github.com>
2024-04-15 17:11:08 -0600

    Implemented UI player status
    Player status values need script management. Attempted player load position fix, by checking if we are in the procedural world

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,10 +18,12 @@ public class Player : MonoBehaviour
         energy = data.energy;
         plantMaterial = data.plantMaterial;
 
+        //Check if we are in the procedural world to load the player's saved position
+        if ("MapGenerationTest" != SceneManager.GetActiveScene().name) { return; }
         Vector3 position;
         position.x = data.position[0];
         position.y = data.position[1];
         position.z = data.position[2];
-        //transform.position = position; //Activate when the scene loading is handled.
+        transform.position = position;
     }
 }
