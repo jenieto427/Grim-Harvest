00348a86b699e6f100d8515dd989b63f76d66bb8
michaelp15 <123104637+michaelp15@users.noreply.github.com>
2024-04-25 21:46:48 -0600

    Cleaning and fixing player/playerdata

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -149,28 +149,7 @@ public class Player : MonoBehaviour
         stimulant = PlayerPrefs.GetInt("Stimulant", 0);  // Default to 0 if not set
         energyBound = PlayerPrefs.GetFloat("EnergyBound", 30);  // Default to 30 if not set
         phytomass = PlayerPrefs.GetInt("Phytomass", 0);  // Default to 0 if not set
-        PlayerPrefs.SetInt("seenTutorial", seenTutorial);
+        seenTutorial = PlayerPrefs.GetInt("seenTutorial", 0);
         //uiManager.UpdateInteractionUI("Loaded player stats");
-    }
-
-    public void SavePlayer()
-    {
-        SaveSystem.SavePlayer(this);
-    }
-    public void LoadPlayer()
-    {
-        PlayerData data = SaveSystem.LoadPlayer();
-
-        energy = data.energy;
-        plantMaterial = data.plantMaterial;
-        stimulant = data.stimulant;
-
-        //Check if we are in the procedural world to load the player's saved position
-        if ("MapGenerationTest" != SceneManager.GetActiveScene().name) { return; }
-        Vector3 position;
-        position.x = data.position[0];
-        position.y = data.position[1];
-        position.z = data.position[2];
-        transform.position = position;
     }
 }
--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,25 +1,123 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[System.Serializable]
-public class PlayerData
+public class PlayerData : MonoBehaviour
 {
-    public float energy;
-    public int plantMaterial;
-    public int stimulant;
-    public float[] position;
+    public float energy = 30;
+    public int plantMaterial = 0;
+    public int stimulant = 0;
+    public float energyBound = 30;
+    public int phytomass = 20; //Start with default 50 mass
+    public int seenTutorial = 0; // 0 Is false
+    public UIManager uiManager;
+    public Player player;
 
-    //Constructor
-    public PlayerData(Player player)
+    void Start()
     {
-        energy = player.energy;
-        plantMaterial = player.plantMaterial;
-        stimulant = player.stimulant;
+        LoadPlayerPrefs();
+    }
+    public void Travel()
+    {
+        DecreaseEnergy(0.5f);
+        // Get the current scene name
+        string currentSceneName = SceneManager.GetActiveScene().name;
 
-        position = new float[3];
-        position[0] = player.transform.position.x;
-        position[1] = player.transform.position.y;
-        position[2] = player.transform.position.z;
+        // Check if the current scene is not 'Village'
+        SceneLoader.LoadScene(currentSceneName == "Village" ? "MapGenerationTest" : "Village");
+    }
+    public void setSeenTutorialFalse()
+    {
+        this.seenTutorial = 0;
+        SavePlayerPrefs();
+    }
+    public void setSeenTutorialTrue()
+    {
+        this.seenTutorial = 1;
+        SavePlayerPrefs();
+
+    }
+    public bool boolSeenTutorial()
+    {
+        if (this.seenTutorial == 0) { return false; }
+        else { return true; }
+    }
+    public void DecrementStimulant()
+    {
+        this.stimulant--;
+        if (this.stimulant < 0) { this.energy = 0; } //Clamp stimulants low bound to 0
+        SavePlayerPrefs();
+    }
+    public void decreasePhytomass(int decreaseAmt)
+    {
+        if (this.phytomass <= 0) { this.phytomass = 2; }
+        this.phytomass -= decreaseAmt;
+    }
+    public void increasePhytomass(int increaseAmt)
+    {
+        if (this.phytomass > 100) { this.phytomass = 100; }
+        this.phytomass += increaseAmt;
+    }
+    public void IncrementStimulant()
+    {
+        this.stimulant++;
+        //if (this.stimulant > 0) { this.energy = 0; } //Upper bound on num of stimulants
+        SavePlayerPrefs();
+    }
+    public void DecreaseEnergy(float decreaseAmt)
+    {
+        this.energy -= decreaseAmt;
+        if (this.energy <= 0) { player.playerDeath(); } //Clamp energy low bound to 0
+        SavePlayerPrefs();
+    }
+    public void IncreaseEnergy(float increaseAmt)
+    {
+        this.energy += increaseAmt;
+        if (this.energy > energyBound) { this.energy = energyBound; } //Clamp energy to high 30 Hz
+        SavePlayerPrefs();
+    }
+    public void DecreasePlantMaterial(int decreaseAmt)
+    {
+        this.plantMaterial -= decreaseAmt;
+        if (this.plantMaterial < 0) { this.plantMaterial = 0; }
+        SavePlayerPrefs();
+    }
+    public void IncreasePlantMaterial(int increaseAmt)
+    {
+        this.plantMaterial += increaseAmt;
+        //Upper bound on plant material
+        //if (this.plantMaterial > 100000) { this.plantMaterial = 100000; }
+        SavePlayerPrefs();
+    }
+    public void resetPlayerPrefs()
+    {
+        this.energy = 30;
+        this.plantMaterial = 0;
+        this.stimulant = 0;
+        this.energyBound = 30;
+        this.phytomass = 50;
+        SavePlayerPrefs();
+    }
+    public void SavePlayerPrefs()
+    {
+        PlayerPrefs.SetFloat("Energy", energy);
+        PlayerPrefs.SetInt("PlantMaterial", plantMaterial);
+        PlayerPrefs.SetInt("Stimulant", stimulant);
+        PlayerPrefs.SetFloat("EnergyBound", energyBound);
+        PlayerPrefs.SetInt("Phytomass", phytomass);
+        PlayerPrefs.SetInt("seenTutorial", seenTutorial);
+        PlayerPrefs.Save();  // Don't forget to call Save to write to disk
+
+        //uiManager.UpdateInteractionUI("Saved player stats");
+    }
+
+    public void LoadPlayerPrefs()
+    {
+        energy = PlayerPrefs.GetFloat("Energy", 30);  // Default to 30 if not set
+        plantMaterial = PlayerPrefs.GetInt("PlantMaterial", 0);  // Default to 0 if not set
+        stimulant = PlayerPrefs.GetInt("Stimulant", 0);  // Default to 0 if not set
+        energyBound = PlayerPrefs.GetFloat("EnergyBound", 30);  // Default to 30 if not set
+        phytomass = PlayerPrefs.GetInt("Phytomass", 0);  // Default to 0 if not set
+        PlayerPrefs.SetInt("seenTutorial", seenTutorial);
+        //uiManager.UpdateInteractionUI("Loaded player stats");
     }
 }
